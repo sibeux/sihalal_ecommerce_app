539ba99f8ba70b7f768986ef3253f54d4437261b
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-09-08 03:57:43 +0700

    Refactor import statements for home screen widgets

--- lib/screens/detail_product_screen/detail_product_screen.dart
+++ lib/screens/detail_product_screen/detail_product_screen.dart
@@ -1,7 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:sihalal_ecommerce_app/controller/product_detail_controller.dart';
