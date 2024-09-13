6e974bd365d1eee18f26626ecf1f09c9bcedd2f0
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-09-13 17:52:11 +0700

    Refactor import statements for home screen widgets and add show/hide functionality for product description

--- lib/controller/product_detail_controller.dart
+++ lib/controller/product_detail_controller.dart
@@ -1,9 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ProductDetailController extends GetxController {
   var imageIndex = 1.obs;
+  var isShowAllDescription = false.obs;
+  var useMaxLine = 5.obs;
+  var useOverflow = RxList<TextOverflow>([TextOverflow.ellipsis]).obs;
 
   void changeImageIndex(int index) {
     imageIndex.value = index;
   }
+
+  void showAllDescription() {
+    isShowAllDescription.value = true;
+    useMaxLine.value = 100;
+    useOverflow.value = RxList<TextOverflow>([TextOverflow.visible]);
+  }
+
+  void showLessDescription() {
+    isShowAllDescription.value = false;
+    useMaxLine.value = 5;
+    useOverflow.value = RxList<TextOverflow>([TextOverflow.ellipsis]);
+  }
+
+  get maxLine => useMaxLine.value;
+  get overflow => useOverflow.value;
 }
--- lib/screens/detail_product_screen/detail_product_screen.dart
+++ lib/screens/detail_product_screen/detail_product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -34,9 +35,9 @@ class DetailProductScreen extends StatelessWidget {
     ];
     final productDetailController = Get.put(ProductDetailController());
     return Scaffold(
-      backgroundColor: HexColor('#fefffe'),
+      backgroundColor: HexColor('#fefeff'),
       appBar: AppBar(
-        backgroundColor: HexColor('#fefffe'),
+        backgroundColor: HexColor('#fefeff'),
         title: const Text('Detail Product'),
       ),
       body: SingleChildScrollView(
@@ -77,7 +78,7 @@ class DetailProductScreen extends StatelessWidget {
                     width: 35,
                     height: 20,
                     decoration: BoxDecoration(
-                      color: HexColor('#fefefe'),
+                      color: HexColor('#fefeff'),
                       borderRadius: BorderRadius.circular(3),
                       border: Border.all(
                         color: const Color.fromARGB(255, 217, 220, 231),
@@ -105,62 +106,135 @@ class DetailProductScreen extends StatelessWidget {
               thickness: 0.5,
             ),
             const SizedBox(
-              height: 8,
+              height: 15,
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Flexible(
                     fit: FlexFit.tight,
                     child: Text(
-                      priceFormat(product.harga),
+                      product.nama,
+                      maxLines: 2,
                       style: const TextStyle(
                         color: Colors.black,
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
                         overflow: TextOverflow.ellipsis,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                  const Icon(
-                    Icons.favorite_border_outlined,
-                    color: Colors.grey,
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Row(
+                    children: [
+                      ProductRating(rating: product.rating),
+                      product.rating == '0.0000'
+                          ? const SizedBox()
+                          : Text(
+                              '(${product.jumlahUlasan})',
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                              ),
+                            ),
+                    ],
                   ),
                 ],
               ),
             ),
             const SizedBox(
-              height: 8,
+              height: 10,
             ),
             Container(
               alignment: Alignment.centerLeft,
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Text(
-                product.nama,
-                maxLines: 3,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
+                priceFormat(product.harga),
+                style: TextStyle(
+                  color: HexColor('#3f44a6'),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                   overflow: TextOverflow.ellipsis,
-                  fontWeight: FontWeight.w500,
                 ),
               ),
             ),
             const SizedBox(
-              height: 12,
+              height: 15,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 10,
+              ),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: HexColor('#f0f2f5'),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  ProductRating(rating: product.rating),
-                  const SizedBox(width: 2),
                   Text(
-                    '${product.jumlahUlasan} Ulasan',
-                    style: const TextStyle(
-                      color: Colors.black,
+                    'Deskripsi Produk',
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(0.8),
                       fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  AutoSizeText(
+                    product.deskripsi,
+                    maxLines: 5,
+                    maxFontSize: 14,
+                    minFontSize: 14,
+                    // Jika memakai AutoSizeText, maka overflow tidak bisa digunakan
+                    // overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(0.6),
+                      fontSize: 14,
+                    ),
+                    overflowReplacement: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Obx(() => Text(
+                              product.deskripsi,
+                              maxLines: productDetailController.maxLine,
+                              overflow: productDetailController.overflow[0],
+                              style: TextStyle(
+                                color: Colors.black.withOpacity(0.6),
+                                fontSize: 14,
+                              ),
+                            )),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            productDetailController.isShowAllDescription.value
+                                ? productDetailController.showLessDescription()
+                                : productDetailController.showAllDescription();
+                          },
+                          child: Obx(() => Text(
+                                productDetailController
+                                        .isShowAllDescription.value
+                                    ? 'Lebih Sedikit'
+                                    : 'Baca Selengkapnya',
+                                style: TextStyle(
+                                  color: HexColor('#3f44a6'),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )),
+                        )
+                      ],
                     ),
                   ),
                 ],
@@ -183,24 +257,14 @@ class ProductRating extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        Container(
-            margin: const EdgeInsets.only(right: 10),
+        SizedBox(
             height: 25,
-            width: 55,
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-              borderRadius: BorderRadius.circular(5),
-              border: Border.all(
-                color: const Color.fromARGB(255, 217, 220, 231),
-                width: 1.1,
-                strokeAlign: BorderSide.strokeAlignOutside,
-              ),
-            ),
+            width: 50,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Icon(Icons.star, color: HexColor('#fec101'), size: 15),
-                const SizedBox(width: 6),
+                const SizedBox(width: 3),
                 Text(
                   rating == '0.0000' ? '---' : ('${double.parse(rating)}'),
                   textAlign: TextAlign.center,
